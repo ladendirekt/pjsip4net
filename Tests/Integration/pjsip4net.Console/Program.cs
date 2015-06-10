2519961147b64b5160294a242410a01391f302ef
Alex McCool <alex.mccool@yahoo.com>
2015-06-09 20:52:41 -0700

    nuget packaging
    nuspec and nupkg

--- Tests/Integration/pjsip4net.Console/Program.cs
+++ Tests/Integration/pjsip4net.Console/Program.cs
@@ -57,32 +57,50 @@ namespace pjsip4net.Console
 
         static void CallManager_CallStateChanged(object sender, CallStateChangedEventArgs e)
         {
+            System.Console.BackgroundColor = ConsoleColor.Blue;
+            System.Console.ForegroundColor = ConsoleColor.White;
             System.Console.WriteLine("Call to {0}: {1}", e.DestinationUri, e.Duration);
+            System.Console.ResetColor();
         }
 
         static void CallManager_IncomingCall(object sender, Core.Utils.EventArgs<Interfaces.ICall> e)
         {
+            System.Console.BackgroundColor = ConsoleColor.Cyan;
+            System.Console.ForegroundColor = ConsoleColor.White;
             System.Console.WriteLine("Incoming call from: {0}", e.Data.RemoteContact);
+            System.Console.ResetColor();
         }
 
         private static void OnNatDetected(object s, NatEventArgs ea)
         {
+            System.Console.BackgroundColor = ConsoleColor.Gray;
+            System.Console.ForegroundColor = ConsoleColor.White;
             System.Console.WriteLine("NAT type detection complete: {0}; {1}", ea.NatTypeName, ea.StatusText);
+            System.Console.ResetColor();
         }
 
         private static void IncomingDtmfDigit(object sender, DtmfEventArgs eventArgs)
         {
+            System.Console.BackgroundColor = ConsoleColor.DarkGreen;
+            System.Console.ForegroundColor = ConsoleColor.White;
             System.Console.WriteLine("Call {0} received {1} digits", eventArgs.CallId, eventArgs.Digit);
+            System.Console.ResetColor();
         }
 
         static void IncomingMessage(object sender, PagerEventArgs e)
         {
+            System.Console.BackgroundColor = ConsoleColor.DarkRed;
+            System.Console.ForegroundColor = ConsoleColor.White;
             System.Console.WriteLine("Message from " + e.From + ", text: " + e.Body);
+            System.Console.ResetColor();
         }
 
         static void CallRedirected(object sender, CallRedirectedEventArgs e)
         {
+            System.Console.BackgroundColor = ConsoleColor.DarkGray;
+            System.Console.ForegroundColor = ConsoleColor.White;
             e.Option = RedirectOption.Accept;
+            System.Console.ResetColor();
         }
     }
 }
