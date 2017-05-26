c3af97017949607494291953403360a44d41071e
AverkinSergei <averkin22@gmail.com>
2017-05-26 15:00:54 +0500

    Update DefaultMediaManager.cs

--- pjsip4net/Media/DefaultMediaManager.cs
+++ pjsip4net/Media/DefaultMediaManager.cs
@@ -143,7 +143,7 @@ namespace pjsip4net.Media
                 _mediaApi.SetCurrentSoundDevices(new Tuple<int, int>(_curCapture.Id, _curPlayback.Id));
             else if (_curCapture == null && _curPlayback != null)
                 _mediaApi.SetCurrentSoundDevices(new Tuple<int, int>(-1, _curPlayback.Id));
-			else if (_curCapture != null && _curPlayback == null)
+	    else if (_curCapture != null && _curPlayback == null)
                 _mediaApi.SetCurrentSoundDevices(new Tuple<int, int>(_curCapture.Id, -1));
             else
                 _mediaApi.SetCurrentSoundDevicesToNull();
